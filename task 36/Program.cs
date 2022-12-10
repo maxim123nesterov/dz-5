312aa30c0a344cdd5002cb6dc4d3302be9d356c7
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-11 02:06:52 +0300

    task 36

---
+++ task 36/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int n = 7;
+int[] arr = new int[n];
+int sum = 0;
+//int j = 0;
+for (int i = 0; i < n; i ++)
+     {
+        arr[i] = new Random().Next(0, 10);
+     Console.Write($" {arr[i]}");
+        if (i % 2 == 1)
+        sum = sum + arr[i];
+     }
+    
+Console.WriteLine();
+Console.Write($"сумма = {sum}");
+    
