735792145af02b0028ee6415ac1506897fd5d609
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-13 12:49:17 +0300

    task 38

---
+++ task 38/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int n = 5;
+int[] arr = new int [n];
+int max = 0;
+int min = 10;
+
+for (int i = 0; i < n; i ++)
+{
+    arr[i] = new Random().Next(0, 10);
+    Console.Write($" {arr[i]}");
+    
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    if (arr[i] < min)
+    {
+       min = arr[i];
+    }
+}
+Console.WriteLine();
+Console.Write($"Максимальное число - {max}");
+Console.WriteLine();
+Console.Write($"Минимальное число - {min}");
+Console.WriteLine();
+Console.Write($"Разница между min и max = {max - min}");
